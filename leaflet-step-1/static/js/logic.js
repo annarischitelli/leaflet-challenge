b9621983e96b8e4655cd035a9d18784961541941
annarischitelli <annarischitelli94@gmail.com>
2021-02-03 19:58:05 -0700

    can't get the ayer to load

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -18,14 +18,14 @@ d3.json(queryUrl).then(function(data) {
     zoomOffset: -1,
     id: "mapbox/streets-v11",
     accessToken: API_KEY
-  });
+  }).addTo(myMap);
 
   var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "dark-v10",
     accessToken: API_KEY
-  });
+  }).addTo(myMap);
 
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
@@ -84,5 +84,5 @@ function pointToLayerFunc(feature, latlng) {
   var earthquakes = L.geoJSON(data.features, {
     onEachFeature: onEachFeatureFunc,
     pointToLayer: pointToLayerFunc
-    
+
   }).addTo(myMap);
